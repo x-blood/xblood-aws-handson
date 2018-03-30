ebe22ee3870955c15afc02ab979c82e571e2125b
x-blood <yasaisato@gmail.com>
2018-03-30 18:02:19 +0900

    Modified some files

--- alexa-reservation-room/src/handlers/index.js
+++ alexa-reservation-room/src/handlers/index.js
@@ -1,23 +1,37 @@
-"use strict";
-const Alexa = require('alexa-sdk'); // Alexa SDKの読み込み
+'use strict';
+var Alexa = require('alexa-sdk');
+
+var APP_ID = undefined;
+
+var SKILL_NAME = "会議室";
+var GET_FACT_MESSAGE = "知ってましたか？";
+var HELP_MESSAGE = "テスト中です。ヘルプメッセージ";
+var HELP_REPROMPT = "どうしますか？";
+var STOP_MESSAGE = "さようなら";
+
 exports.handler = function(event, context, callback) {
-
-var alexa = Alexa.handler(event, context);
-
-    // Alexa SDKの処理
-    alexa.appId = process.env.APP_ID;
+    var alexa = Alexa.handler(event, context);
+    alexa.APP_ID = APP_ID;
     alexa.registerHandlers(handlers);
     alexa.execute();
+};
 
-    // ハンドラの定義
-    var handlers = {
-       'WeatherIntent': function () {
-            var city = this.event.request.intent.slots.City.value;
-            var weather = weatherFunc(city);
-            this.emit(':tell', '本日の' + city + 'の天気は' + weather + 'です');
+var handlers = {
+    'LaunchRequest': function () {
+        this.emit('GetNewFactIntent');
     },
-    'LaunchRequest': function () {...},
-    'AMAZON.HelpIntent': function () {...},
-    'AMAZON.StopIntent': function () {...},
-    'SessionEndedRequest ': function () {...}
+    'GetNewFactIntent': function () {
+        this.emit(':tellWithCard', "すいません、まだ開発中で、その要望には答えられません。")
+    },
+    'AMAZON.HelpIntent': function () {
+        var speechOutput = HELP_MESSAGE;
+        var reprompt = HELP_REPROMPT;
+        this.emit(':ask', speechOutput, reprompt);
+    },
+    'AMAZON.CancelIntent': function () {
+        this.emit(':tell', STOP_MESSAGE);
+    },
+    'AMAZON.StopIntent': function () {
+        this.emit(':tell', STOP_MESSAGE);
+    }
 };

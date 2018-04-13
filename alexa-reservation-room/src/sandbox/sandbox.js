d12c1edbdf7a24b2603fc44c3d3ca4fd06bb2f2a
x-blood <yasaisato@gmail.com>
2018-04-13 13:55:53 +0900

    Modified sandbox.js

--- alexa-reservation-room/src/sandbox/sandbox.js
+++ alexa-reservation-room/src/sandbox/sandbox.js
@@ -12,3 +12,22 @@ http.get("http://www.google.com", function(res) {
 }).on('error', function(e) {
     console.log('error : ' + e);
 });
+
+requestFunction(function requestCallback(err) {
+  // If error occurs during http.get request - respond with console.log
+  if (err) {
+    console.log('HTTP Error: request not sent');
+  }
+  // ContinueIntent(session,response);
+});
+
+function requestFunction(requestCallback){
+  var url = "http://www.google.com";
+
+  http.get(url, function(res) {
+    console.log("Got response: " + res.statusCode);
+    requestCallback(null);
+  }).on('error', function (e) {
+    console.log("Got error: ", e);
+  });
+}
